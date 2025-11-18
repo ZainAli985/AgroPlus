09ecc852ee323e38c28936bd82d23c3218494feb
ZainAli985 <zain20061008@gmail.com>
2025-11-18 18:57:45 +0500

    ADDED UPDATED RAILWAY LINK

--- backend/config/API_BASE_URL.js
+++ backend/config/API_BASE_URL.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'https://rehman-rice-mill-production.up.railway.app/api'
+const API_BASE_URL = 'https://steadfast-nurturing-production.up.railway.app/api' 
+// 'https://rehman-rice-mill-production.up.railway.app/api'
 
 export default API_BASE_URL;
