17cfa15980a9f25efc6333dcee852faa9a795b0b
ZainAli985 <zainalibusiness05@gmail.com>
2025-12-30 14:01:25 +0500

    CHANGED CATCH ALL ROUTES SPA-5 ISSUE

--- backend/server/server.js
+++ backend/server/server.js
@@ -33,10 +33,10 @@ app.get("/", (_req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
-// 🔥 Catch-all for React routes (REQUIRED)
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(distPath, "index.html"));
-});
+// // 🔥 Catch-all for React routes (REQUIRED)
+// app.get("*", (_req, res) => {
+//   res.sendFile(path.join(distPath, "index.html"));
+// });
 
 // Connect DB
 connectDB();
