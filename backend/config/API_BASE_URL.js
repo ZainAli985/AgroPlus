0f3745cabddcf5145e6e2bea09b9b2210badfc2f
ZainAli985 <zainalibusiness05@gmail.com>
2026-01-20 14:39:02 +0500

    ADDED NEW API BASE URL

--- backend/config/API_BASE_URL.js
+++ backend/config/API_BASE_URL.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'https://rehman-rice-mill-production-7394.up.railway.app/api'; 
+const API_BASE_URL = 'https://rehman-rice-mill-production.up.railway.app/api'; 
+
 
 export default API_BASE_URL;
